def069d1283aac6d581f2c7346b764f2c01aed41
Brance Pearson <brancepearson@gmail.com>
2017-10-31 16:38:36 -0700

    changin JS to constructor functions

--- app.js
+++ app.js
@@ -1,44 +1,17 @@
 'use strict'
-const airport = {
-    name: 'Portland Airport',
-    min: 23,
-    max: 65,
-    avg: 6.3,
-    hourlySales: hourlySales
+function store (name, min, max, avg) {
+    this.name = name,
+    this.min = min,
+    this.max = max,
+    this.avg = avg,
+    this.hourlySales = hourlySales
 };
 
-const pioneer = {
-    name: 'Pioneer Square',
-    min: 3,
-    max: 24,
-    avg: 1.2,
-    hourlySales: hourlySales
-};
-
-const powells = {
-    name: 'Powells',
-    min: 11,
-    max: 38,
-    avg: 3.7,
-    hourlySales: hourlySales
-};
-
-const stjohns = {
-    name: 'St Johns',
-    min: 20,
-    max: 38,
-    avg: 2.3,
-    hourlySales: hourlySales
-};
-
-const waterfront = {
-    name: 'Waterfront',
-    min: 2,
-    max: 16,
-    avg: 4.6,
-    hourlySales: hourlySales
-};
-
+const airport = ('Portland Airport' , 23 , 65 , 6.3);
+const pioneer = ('Pioneer Square' , 3 , 24 , 1.2);
+const powells = ('Powell\'s Bookstore' , 11 , 38 , 3.7);
+const stjohns = ('St Johns' , 20 , 38 , 2.3);
+const waterfront = ('Waterfront' , 2 , 16 , 4.6);
 
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
@@ -64,38 +37,22 @@ function hourlySales(){
     }
     this.salesByHour = salesByHour;
 
-    // table head
-    const thead = document.createElement('THEAD');
-    table.appendChild(thead);
-    thead.textContent = this.name;
     
-    //table row
-    const trow = document.createElement('TR');
-    table.appendChild(trow);
-    trow.textContent = this.hours;
-
-    const hourData = document.createElement('TD');
-    trow.appendChild(hourData);
-    hourData.textContent = "Hours: " + this.salesByHour.hour;
-    
-    const cookieData = document.createElement('TD');
-    trow.appendChild(cookieData);
-    cookieData.textContent = "Cookies: " + this.salesByHour.cookiesSold;
     
 
-    // // Original render code
-    // const mainList = document.getElementById('list');
-    // const title = document.createElement('H3');
-    // title.textContent = this.name;
-    // const ul = document.createElement('UL');
-    // mainList.appendChild(ul);
-    // ul.appendChild(title);
+    // Original render code
+    const mainList = document.getElementById('list');
+    const title = document.createElement('H3');
+    title.textContent = this.name;
+    const ul = document.createElement('UL');
+    mainList.appendChild(ul);
+    ul.appendChild(title);
 
-    // for (let i= 0; i < hours.length; i++) {
-    //     let li = document.createElement('LI');
-    //     li.textContent = "Hour: " + this.salesByHour[i].hour + " Cookies: " + this.salesByHour[i].cookiesSold;
-    //     ul.appendChild(li);
-    // }
+    for (let i= 0; i < hours.length; i++) {
+        let li = document.createElement('LI');
+        li.textContent = "Hour: " + this.salesByHour[i].hour + " Cookies: " + this.salesByHour[i].cookiesSold;
+        ul.appendChild(li);
+    }
 }
 
 const mainList = document.getElementById('list');
@@ -106,6 +63,25 @@ mainList.appendChild(table);
 // const thead = document.createElement('THEAD');
 // table.appendChild(thead);
 // thead.textContent = "Hours: ";
+
+
+// // table head
+// const thead = document.createElement('THEAD');
+// table.appendChild(thead);
+// thead.textContent = this.name;
+
+// //table row
+// const trow = document.createElement('TR');
+// table.appendChild(trow);
+// trow.textContent = this.hour;
+
+// const hourData = document.createElement('TD');
+// trow.appendChild(hourData);
+// hourData.textContent = "Hour: ";
+
+// const cookieData = document.createElement('TD');
+// trow.appendChild(cookieData);
+// cookieData.textContent = "Cookies: ";
 
 
 airport.hourlySales();
