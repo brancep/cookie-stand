dbff8accd6b63b75c62ccbf5e6e326c8da59d542
Brance Pearson <brancepearson@gmail.com>
2017-10-30 17:37:25 -0700

    this will complete the JS code and render a list item to the page.

--- app.js
+++ app.js
@@ -1,58 +1,77 @@
 'use strict'
 
-// const locations = [ 'airport' , 'pioneer' , 'powells' , 'stjohns' , 'waterfront']
-const hours = ['00:00' , '01:00' , '02:00' , '03:00' , '04:00' , '05:00' , '06:00' , '07:00' , '08:00' , '09:00']
+render();
+function render () {
+    const ul = document.getElementById('main-list');
+    const li = document.createElement('li');
+    li.textContent = "List item";
+    ul.appendChild(li);
+}
+
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 
-var calcSales = function getRandomIntInclusive(min,max) {
+function getRandomIntInclusive(min,max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const airport = {
+    name: 'Portland Airport',
     min: 23,
     max: 65,
     avg: 6.3,
-    calcSales: calcSales
+    hourlySales: hourlySales,
 };
 
-
-for (i = 0; i < hours.length; i++) {
-    function cookies () {
-        const avgCust = airport.calcSales(airport.min , airport.max);
-        const cookiePerHour = avgCust * airport.avg;
+function hourlySales(){
+    const salesByHour = [];
+    for (let i=0; i < hours.length; i++){
+        const hour = hours[i];
+        const numCustomers = getRandomIntInclusive(this.min, this.max);
+        const numCookies = Math.round(numCustomers * this.avg);
+        const oneHour = {
+            hour: hour,
+            cookiesSold: numCookies
+        }
+        salesByHour.push(oneHour);
     }
-
-    console.log(cookies);
+    this.salesByHour = salesByHour;
 }
+
+airport.hourlySales();
+console.log(airport);
 
 
 // const pioneer = {
+//     name: 'Pioneer Square',
 //     min: 3,
 //     max: 24,
 //     avg: 1.2,
+//     hourlySales: hourlySales
 // };
 
 // const powells = {
+//     name: 'Powells',
 //     min: 11,
 //     max: 38,
 //     avg: 3.7,
+//     hourlySales: hourlySales
 // };
 
 // const stjohns = {
+//     name: 'St Johns',
 //     min: 20,
 //     max: 38,
 //     avg: 2.3,
+//     hourlySales: hourlySales
 // };
 
 // const waterfront = {
+//     name: 'Waterfront',
 //     min: 2,
 //     max: 16,
 //     avg: 4.6,
+//     hourlySales: hourlySales
 // };
-
-// for (i = 0; i < hours.length; i++) {
-      
-// }
-
