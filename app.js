5d607e46a9ef50ce5b8573b0306f2b6dae667e75
Brance Pearson <brancepearson@gmail.com>
2017-11-02 16:24:17 -0700

    this will finalize the totals and display them on the table. Also, update the header with CSS.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const powells = new Store ('Powell\'s Bookstore' , 11 , 38 , 3.7);
 const stjohns = new Store ('St Johns' , 20 , 38 , 2.3);
 const waterfront = new Store ('Waterfront' , 2 , 16 , 4.6);
 
+
+//// Main constructor function for calculating the number of cookies and random number of customers.
 function Store (name, min, max, avg) {
     this.name = name;
     this.min = min;
@@ -24,27 +26,20 @@ function Store (name, min, max, avg) {
             cookiesSold: numCookies
         }
         this.salesByHour.push(oneHour);
-    }
+    }   
 }
 
-
-//Random customer number generator.
-function getRandom (min,max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//// This will display the row of hours at the top.
-
+//// Creation of the main table and row
 const mainList = document.getElementById('list');
 const tRow = document.createElement('TR');
 mainList.appendChild(tRow);
 
+//// Creating the space at the beginning to align rows with hours
 const space = document.createElement('TH');
 space.textContent = "";
 tRow.appendChild(space);
 
+//// This will display the row of hours at the top.
 for (let i=0; i < hours.length; i++) {
     const tHead = document.createElement('TH');
     tHead.textContent = hours[i];
@@ -52,30 +47,69 @@ for (let i=0; i < hours.length; i++) {
 };
 
 
+// salesTotal = 0;
+// for (let i = 0; i < hours.length; i ++) {
+//     let salesTotal = this.salesTotal + this.salesByHour.cookiesSold;
+// }
+
+
+/// Total header
+const totalHeader = document.createElement('TH');
+tRow.appendChild(totalHeader);
+totalHeader.textContent = "Total";
+
+
 //// Main proto for building out data in the columns.
 Store.prototype.build = function () {
 
-    // This is for location names. Need to sort our how to have them on the left.
-    
+    //// Location Names
     const trCookie = document.createElement('TR'); // TR create
     mainList.appendChild(trCookie);
     const locNames = document.createElement('TH');
     locNames.textContent = this.name;
     trCookie.appendChild(locNames);
 
+    //// Loop for the data and rendering the data on the page.
     for (let i= 0; i < this.salesByHour.length; i++) {
         const tdCookie = document.createElement('TD'); // TD create
         tdCookie.textContent = this.salesByHour[i].cookiesSold; // TD content
         trCookie.appendChild(tdCookie);
     }
+
+    let totalData = 0;
+    for (let i= 0; i < hours.length; i++) {
+        totalData = totalData + this.salesByHour[i].cookiesSold;
+    }
+
+    const tSalesData = document.createElement('TH');
+    tSalesData.textContent = totalData;
+    console.log(tSalesData);
+    trCookie.appendChild(tSalesData);
 }
 
+
+
+//// HELPER FUNCTIONS ////
+//Random customer number generator.
+function getRandom (min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+//// Calling the functions.
 
 airport.build();
 pioneer.build();
 powells.build();
 stjohns.build();
 waterfront.build();
+
+
+
+
+//// Taking the information from the for and populating it to the page.
 
 const formInfo = document.getElementById('form');
 
